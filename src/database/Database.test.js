293a26033ecbb63b686cef9acd9034d725d79672
deepeshpakhare <deepeshpakhare@gmail.com>
2024-01-06 12:25:38 +0530

    test: saveQeustionAnswerTest included

--- src/database/Database.test.js
+++ src/database/Database.test.js
@@ -1,22 +1,36 @@
 import { _saveQuestion } from "./Database";
+import { _saveQuestionAnswer } from "./Database";
 
+
+// _saveQuestion() test
 const question = {
     optionOneText: "choose monolithic architecture",
     optionTwoText: "choose microservices architecture",
     author: "sarahedo"
 }
 
-test(`the data is ${question}`, async () => {
-    const data = await _saveQuestion(question);
+test(`the result is ${question}`, async () => {
+    const result = await _saveQuestion(question);
     const toTest = {
-        optionOneText: data.optionOne.text,
-        optionTwoText: data.optionTwo.text,
-        author: data.author,
+        optionOneText: result.optionOne.text,
+        optionTwoText: result.optionTwo.text,
+        author: result.author,
     }
     expect(toTest).toStrictEqual(question);
 }
 );
 
 test('the fetch fails with a message Please provide optionOneText, optionTwoText, and author', async () => {
-    await expect(_saveQuestion({author:"sarahedo"})).rejects.toMatch('Please provide optionOneText, optionTwoText, and author');
+    await expect(_saveQuestion({ author: "sarahedo" })).rejects.toMatch('Please provide optionOneText, optionTwoText, and author');
+});
+
+
+//_saveQuestionAnswer() test
+test("the result is a true value", async () => {
+    await expect(_saveQuestionAnswer({ authedUser: "sarahedo", qid: "vthrdm985a262al8qx3do", answer: "optionOne" })).resolves.toBeTruthy();
+}
+);
+
+test('the fetch fails with a false value', async () => {
+    await expect(_saveQuestionAnswer({ authedUser: "sarahedo" })).rejects.toMatch("Please provide authedUser, qid, and answer");
 });
