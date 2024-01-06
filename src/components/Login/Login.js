f29e647a3dca807e87e4501c837eefea707c0fe2
deepeshpakhare <deepeshpakhare@gmail.com>
2024-01-06 16:13:21 +0530

    style: login collage-image added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -16,7 +16,7 @@ export default function Login() {
   const users = useSelector((state)=>state.auth.authDetails);
   const dispatch = useDispatch();
   const [password, setPassword] = useState("");
-  const [selectedUser, setSelectedUser] = useState({avatarURL:"sarahedo.jpg"});
+  const [selectedUser, setSelectedUser] = useState({avatarURL:"employee-collage.jpg"});
   const [showPasswordField, setShowPasswordField] = useState(false);
   const navigate = useNavigate();
 
